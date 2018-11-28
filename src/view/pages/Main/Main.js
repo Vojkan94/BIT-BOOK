0ef28382465c013fd830540fcba34320ee178d0d
student <student@bgit.rs>
2018-11-28 17:22:28 +0100

    [book-0] App navigation

---
+++ src/view/pages/Main/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div className="container mt-5">
+            <p>Nothing in feed...</p>
+        </div>
+    )
+}
+
+export default Main
