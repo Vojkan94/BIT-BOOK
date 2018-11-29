b795dfa0d6a1a1c9bcce2c58f767aa4940e4f260
BIT Student <classroom@bgit.rs>
2018-11-29 13:51:05 +0100

    add link to header

--- src/view/components/common/Header.js
+++ src/view/components/common/Header.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <header className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container">
-                <a className="navbar-brand" href="#!">BIT BOOK</a>
+                <Link className="navbar-brand" to="/">BIT BOOK</Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse " id="navbarNav">
                     <ul className="navbar-nav ml-auto">
                         <li className="nav-item">
-                            <a className="nav-link" href="#!">Feed</a>
+                            <Link className="nav-link" to="/">Feed</Link>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="#!">People</a>
