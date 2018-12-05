c002584595a90e3a7c85c8a72fd16832f7008d62
BIT Student <classroom@bgit.rs>
2018-12-05 17:24:10 +0100

    view pagination on feed

--- src/view/components/feed/PostList.js
+++ src/view/components/feed/PostList.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PostItem from './PostItem';
 import PostFilter from './PostFilter';
 import ScreenLoading from '../ScreenLoading/ScreenLoading';
-
+import { Link } from 'react-router-dom';
 import { postService } from '../../../services/postService';
 import { userService } from '../../../services/userService';
 import { NewPostButton } from "./../feed/ButtonFeed/NewPostButton"
@@ -53,11 +53,24 @@ class PostList extends Component {
                 return post.type === this.state.selectValue;
             }).map((post) => <PostItem key={post.id} post={post} userId={this.state.userId} loadPosts={this.loadPosts} />)
         }
+
+        const tenPosts = [];
+        for (let i = 0; i <= 10; i++) {
+            const postItem = postList[i];
+            tenPosts.push(postItem)
+        }
         return (
             <>
                 <div className="row">
                     <div className="col-12 col-md-10 col-lg-8 offset-md-2">
-                        {postList}
+                        {tenPosts}
+                        <ul class="pagination">
+                            <li class="page-item"><a class="page-link" href="#!">Previous</a></li>
+                            <li class="page-item"><a class="page-link" href="#!">1</a></li>
+                            <li class="page-item"><a class="page-link" href="#!">2</a></li>
+                            <li class="page-item"><a class="page-link" href="#!">3</a></li>
+                            <li class="page-item"><a class="page-link" href="#!">Next</a></li>
+                        </ul>
                     </div>
                     <div className="col-2">
                         <PostFilter changeHandler={this.changeHandler} />
