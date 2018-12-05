af01b7205394a52f2374b10af8bc856bea5db26a
student <student@bgit.rs>
2018-12-05 10:54:00 +0100

    Implement user img in comment

--- src/view/components/singlePost/comment/CommentList.js
+++ src/view/components/singlePost/comment/CommentList.js
@@ -2,26 +2,62 @@ import React, { Component } from 'react';
 import CommentItem from './CommentItem';
 import CommentInput from './CommentInput';
 import { postService } from '../../../../services/postService';
+import { userService } from '../../../../services/userService';
 
 class CommentList extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            comments: []
+            comments: [],
+            users: [],
+            commentUsers: []
         }
         this.loadComments = this.loadComments.bind(this);
     }
-    loadComments() {
+    loadComments = () => {
         postService.fetchComments(this.props.postId)
             .then((comments) => {
                 this.setState({
                     comments
                 })
+                this.loadUsers();
+            })
+
+    }
+
+    loadUsers = () => {
+        userService.fetchUsers()
+            .then((users) => {
+                this.setState({
+                    users
+                })
+                this.getComments();
             })
     }
+
+
+    getComments = () => {
+
+        const userComments = this.state.users.filter((user) => {
+            const comments = this.state.comments;
+            let valid;
+            for (let i = 0; i < comments.length; i++) {
+                const comment = comments[i];
+                valid = comment.authorId === user.id
+                if (valid) { break }
+            }
+            return valid
+        })
+        this.setState({
+            commentUsers: userComments
+        })
+        console.log(userComments);
+
+    }
     componentDidMount() {
-        this.loadComments()
+        this.loadComments();
+
     }
 
 
@@ -35,7 +71,7 @@ class CommentList extends Component {
             )
         }
         const commentItem = this.state.comments.map((comment) => {
-            return <CommentItem key={comment.id} comment={comment} />
+            return <CommentItem key={comment.id} comment={comment} users={this.state.commentUsers} />
         })
         return (
             <>
