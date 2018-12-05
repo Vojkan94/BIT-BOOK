1c4aae72707349b6e04c7998ce5a262d7a80aaca
BIT Student <classroom@bgit.rs>
2018-12-05 11:22:51 +0100

    implement bind for register and implement success message

--- src/view/components/welcome/WelcomeList.js
+++ src/view/components/welcome/WelcomeList.js
@@ -10,10 +10,12 @@ class WelcomeList extends Component {
 
         this.state = {
             loginActive: true,
-            registerActive: false
+            registerActive: false,
+            message: '',
         }
         this.loginBtn = this.loginBtn.bind(this);
         this.registerBtn = this.registerBtn.bind(this);
+        this.successRegister = this.successRegister.bind(this);
     }
 
     loginBtn() {
@@ -28,14 +30,21 @@ class WelcomeList extends Component {
             registerActive: true
         })
     }
+    successRegister() {
+        this.setState({
+            message: 'You are successfully registration, please log in now'
+        })
 
-
-
+    }
     render() {
         const isActiveLogin = this.state.loginActive ? 'active' : '';
         const isActiveRegister = this.state.registerActive ? 'active' : '';
+        const isSuccess = !this.state.message ? 'd-none' : '';
         return (
             <div className="col-5">
+                <div className={`alert alert-success ${isSuccess}`} role="alert">
+                    {this.state.message}
+                </div>
                 <nav>
                     <div className="nav nav-tabs nav-login" id="nav-tab" role="tablist">
                         <button onClick={this.loginBtn} className={`nav-item nav-link ${isActiveLogin}`} id="nav-home-tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Login</button>
@@ -44,14 +53,11 @@ class WelcomeList extends Component {
                 </nav>
                 <div className="tab-content" id="nav-tabContent">
                     <WelcomeLogin isActive={this.state.loginActive} />
-                    <WelcomeRegister isActive={this.state.registerActive} loginBtn={this.loginBtn} />
+                    <WelcomeRegister isActive={this.state.registerActive} loginBtn={this.loginBtn} successRegister={this.successRegister} />
                 </div>
 
             </div>
-
-
         )
     }
 }
-
-export default WelcomeList
+export default WelcomeList;
--- src/view/components/welcome/WelcomeRegister.js
+++ src/view/components/welcome/WelcomeRegister.js
@@ -7,36 +7,41 @@ class WelcomeRegister extends Component {
         super(props);
         this.state = {
             nameInput: '',
-            userNameInput: '',
+            usernameInput: '',
             emailInput: '',
             passwordInput: '',
             error: ''
         }
+        this.changeNameInput = this.changeNameInput.bind(this);
+        this.changeUsernameInput = this.changeUsernameInput.bind(this);
+        this.changeEmailInput = this.changeEmailInput.bind(this);
+        this.changePasswordInput = this.changePasswordInput.bind(this);
+        this.registerUser = this.registerUser.bind(this);
     }
 
-    changeNameInput = (event) => {
+    changeNameInput(event) {
         this.setState({
             nameInput: event.target.value
         })
     }
-    changeUserNameInput = (event) => {
+    changeUsernameInput(event) {
         this.setState({
-            userNameInput: event.target.value
+            usernameInput: event.target.value
         })
     }
-    changeEmailInput = (event) => {
+    changeEmailInput(event) {
         this.setState({
             emailInput: event.target.value
         })
     }
-    changePasswordInput = (event) => {
+    changePasswordInput(event) {
         this.setState({
             passwordInput: event.target.value
         })
     }
-    registerUser = () => {
+    registerUser() {
         const inputData = {
-            'username': this.state.userNameInput,
+            'username': this.state.usernameInput,
             'password': this.state.passwordInput,
             'name': this.state.nameInput,
             'email': this.state.emailInput
@@ -44,39 +49,30 @@ class WelcomeRegister extends Component {
         userService.createUser(inputData)
             .then((response) => {
                 if (response.sessionId) {
-                    this.setState({
-                        error: ''
-
-                    })
+                    this.setState({ error: '' })
                     this.props.loginBtn();
-
+                    this.props.successRegister();
                 }
                 else {
-                    this.setState({
-                        error: response.error.message
-                    })
+                    this.setState({ error: response.error.message })
                 }
             })
     }
-
     render() {
         const isActive = this.props.isActive ? 'active' : '';
         return (
             <div className={`tab-pane fade show  ${isActive}`} role="tabpanel" aria-labelledby="nav-profile-tab">
                 <div className="row">
                     <form className="col-12 mt-1" id="loginForm">
-
                         <div className="form-group">
                             <input type="text" value={this.state.nameInput} onChange={this.changeNameInput} className="form-control mt-3" id="registerName" placeholder="Enter Name" />
                         </div>
                         <div className="form-group">
-                            <input type="text" value={this.state.userNameInput} onChange={this.changeUserNameInput} className="form-control mt-3" id="registerUserName" placeholder="Enter Username" />
+                            <input type="text" value={this.state.usernameInput} onChange={this.changeUsernameInput} className="form-control mt-3" id="registerUserName" placeholder="Enter Username" />
                         </div>
-
                         <div className="form-group">
                             <input type="text" value={this.state.emailInput} onChange={this.changeEmailInput} className="form-control mt-3" id="registerEmail" placeholder="Enter Email" />
                         </div>
-
                         <div className="form-group">
                             <input type="password" value={this.state.passwordInput} onChange={this.changePasswordInput} className="form-control" id="registerPassword" placeholder="Enter Password" />
                         </div>
@@ -85,9 +81,7 @@ class WelcomeRegister extends Component {
                     </form>
                 </div>
             </div>
-
         )
     }
 }
-
-export default WelcomeRegister
+export default WelcomeRegister;
